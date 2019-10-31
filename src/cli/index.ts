b3a01b0617006fc503ec25fb2516d3aee0f86cdd
Craig Spence <craigspence0@gmail.com>
2019-10-31 20:58:55 +0100

    (refactoring :hammer:) - deleting setup script

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,17 +2,10 @@ import minimist from 'minimist';
 
 import { logError } from '../errors';
 
-import { setup } from './setup';
 import { start } from './start';
 
 export async function cli(argv: Array<string>): Promise<void> {
-  const [first, ...rest] = argv;
-
   try {
-    if (first === 'setup') {
-      await setup(minimist(rest));
-      return;
-    }
     await start(minimist(argv));
     return;
   } catch (e) {
